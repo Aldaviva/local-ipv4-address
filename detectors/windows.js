b82e308568a4fc5f3b362adf08f07daddc26c931
Ben Hutchison <ben@aldaviva.com>
2017-04-22 02:26:43 -0700

    Fixes #1: Does not work when Windows is set to non-English language pack

--- detectors/windows.js
+++ detectors/windows.js
@@ -1,7 +1,7 @@
 var childProcess = require('child_process');
 var Q            = require('q');
 
-var DEFAULT_GATEWAY_LINE_PATTERN = /IPv4 Route Table[^]*Active Routes:[^]*Metric\r?\n\s*(.*?)\s*\n=======/m;
+var DEFAULT_GATEWAY_LINE_PATTERN = /\n(?:\s*0\.0\.0\.0\s*){2}(.*?)\r?\n=========/m;
 
 module.exports.detectLocalIpV4Address = function(){
     return Q.nfcall(childProcess.execFile, "route", ["print", "-4", "0.0.0.0"])
@@ -9,7 +9,7 @@ module.exports.detectLocalIpV4Address = function(){
             var matches = stdout.match(DEFAULT_GATEWAY_LINE_PATTERN);
             if(matches){
                 var columns = matches[1].split(/\s+/);
-                var ipAddress = columns[3];
+                var ipAddress = columns[1];
                 return ipAddress;
             } else {
                 throw new Error("Could not find default gateway route in routing table.");
